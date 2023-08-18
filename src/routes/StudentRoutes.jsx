ffe068716e69c0535d4ae7ab26428bdd260e3961
silvaPool <silva.friends22@gmail.com>
2023-08-18 15:39:15 -0300

    Atualizacao studentRoutes

--- src/routes/StudentRoutes.jsx
+++ src/routes/StudentRoutes.jsx
@@ -1,9 +1,7 @@
 import { Navigate } from "react-router-dom";
 import Principal from "../components/Principal";
-import Conteudo from "../pages/Conteudo";
 import Perfil from "../pages/Perfil";
 import Cards from "../pages/estudante/Cards";
-// import HomeEstudante from "../pages/estudante/HomeEstudante";
 import HomeEstudanteDrawer from "../pages/estudante/HomeEstudanteDrawer";
 import StudentWorkoutDetails from "../pages/estudante/WorkoutStudentDetails";
 import AuthGuard from "../utils/AuthGuard";
